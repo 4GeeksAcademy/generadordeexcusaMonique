f5c7d5f26553c582f01bd32fa6d103ff804ec7a0
Monique Menezes <122538719+Niquemenezes@users.noreply.github.com>
2024-10-01 15:05:56 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let quien = ["Mi tia", "Mi hermana", "Mi gato", "El vecino"];
+  let accion = ["robo", "perdio", "rompio", "tiro"];
+  let que = ["mi llave", "mi telefono", "mi ordenador"];
+  let cuando = ["ayer", "hoy por la mañana", "por la tarde", "el mes pasado"];
+
+  function generarExcusa() {
+    let quienAleatorio = quien[Math.floor(Math.random() * quien.length)];
+    let accionAleatoria = accion[Math.floor(Math.random() * accion.length)];
+    let queAleatorio = que[Math.floor(Math.random() * que.length)];
+    let cuandoAleatorio = cuando[Math.floor(Math.random() * cuando.length)];
+
+    return `${quienAleatorio} ${accionAleatoria} ${queAleatorio} ${cuandoAleatorio}.`;
+  }
+  document.getElementById("excuse").innerText = generarExcusa();
 };
+
+document.getElementById("refreshButton").addEventListener("click", function() {
+  location.reload();
+});
